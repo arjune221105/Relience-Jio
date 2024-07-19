9d3f4ec06b74f57947bc0d2ded204d29d09c384a
arjune221105 <shakti221105@gmail.com>
2024-07-19 01:42:50 -0700

    ToCreate Manager and Employee

---
+++ JioWrokDummpyProject/src/test/java/Employee/ToCreateEmployee.java
@@ -0,0 +1,9 @@
+package Employee;
+
+public class ToCreateEmployee {
+	public static void main(String[] args) {
+		System.out.println("Employee are Working Every-Day 1K");
+		System.out.println("In Jio Company");
+	}
+
+}
---
+++ JioWrokDummpyProject/src/test/java/Manager/ToCreateManager.java
@@ -0,0 +1,10 @@
+package Manager;
+
+public class ToCreateManager {
+
+	public static void main(String[] args) {
+		System.out.println("Every Department Select Manager");
+		System.out.println("Total Manager Select Are Each and Every Department");
+		System.out.println("Count of Manager -:103");
+	}
+}
